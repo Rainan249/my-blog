7498d9c648164e2f7e39ad3fb73fd105c4266375
rainan249 <2913756838@qq.com>
2026-06-11 21:04:28 +0800

    fix: hide footer on blog post page to prevent TOC overlap

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
+const route = useRoute()
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
     <Navbar />
-    <main class="flex-1 pt-16">
+    <main class="flex-1" :class="route.name === 'blog-post' ? '' : 'pt-16'">
       <router-view />
     </main>
-    <Footer />
+    <Footer v-if="route.name !== 'blog-post'" />
   </div>
 </template>
